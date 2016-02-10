fe57e23d898a3b8b219a842752ff210c960c1d48
Kumar Shubham <kumarshubham347@gmail.com>
2016-02-10 22:43:59 +0530

    Added back to top to all pages

---
+++ js/backtotop.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 250) {
+      $("#fixed-back").slideDown();
+    } else {
+      $("#fixed-back").slideUp();
+    }
+  });
+});
