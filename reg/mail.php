c135cb08a4e0e869b09fd927856ce5c33c46f2e3
prashant <pkpbhardwaj729@gmail.com>
2016-02-19 21:29:18 +0530

    new mail test2

--- reg/mail.php
+++ reg/mail.php
@@ -2,10 +2,16 @@
 <?php require_once("includes/db_connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php
-	$email = $_SESSION["username"];
-	$message = "hello friend.";
-	mail($email, 'check', $message);
+	if (isset($_POST['submit'])) {
+		$email = $_SESSION["username"];
+		$message = "hello friend.";
+		mail($email, 'check', $message);
+		echo "Mail sent to ".$email;
+	}
 ?>
+<form action="mail.php" method="post">
+	<input type="submit" name="submit" value="submit">
+</form>
 <?php
 	if (isset ($conn)){
 	  mysqli_close($conn);
