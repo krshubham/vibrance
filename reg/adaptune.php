15c11cb4bd01cb436c14356ede4bc2c94e5072c0
prashant <pkpbhardwaj729@gmail.com>
2016-02-19 13:48:07 +0530

    check test2

--- reg/adaptune.php
+++ reg/adaptune.php
@@ -21,19 +21,17 @@
 	while ($check_title = mysqli_fetch_assoc($check_result)) {
 		if ($check_title['email']==$email) {
 			echo "You have already registered for adaptune.";
-		} break;
-		 else {
-			$query =$check_title['email']==$email "INSERT INTO adaptune (name, email, college, regno, phno)";
-			$query .= " VALUES ('{$name}', '{$email}', '{$college}', '{$regno}', '{$phno}')";
-			$result = mysqli_query($conn, $query);
+		}		 
+	}
+	$query =$check_title['email']==$email "INSERT INTO adaptune (name, email, college, regno, phno)";
+	$query .= " VALUES ('{$name}', '{$email}', '{$college}', '{$regno}', '{$phno}')";
+	$result = mysqli_query($conn, $query);
 
-		    if ($result) {
-		      	echo"You have succesfully registered for adaptune. Please check your email for registraion slip. Your registraion will only be confirmed after you make the payment at our registration desk.";	       	
-		    } else {
-			   	echo"Registration failed.";
-		    }  
-		}
-	}  	
+    if ($result) {
+      	echo"You have succesfully registered for adaptune. Please check your email for registraion slip. Your registraion will only be confirmed after you make the payment at our registration desk.";	       	
+    } else {
+	   	echo"Registration failed.";
+    }    	
 ?>      	
   
 <?php
