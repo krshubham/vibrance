22a21e3f7e1ba950613a166e227530cc8c1475d1
prashant <pkpbhardwaj729@gmail.com>
2016-02-19 20:57:08 +0530

    mail test4

--- reg/adaptune.php
+++ reg/adaptune.php
@@ -14,7 +14,7 @@
 	$college = $name_title['college'];
 	$regno = $name_title['regno'];
 	$phno = $name_title['phno'];
-	$message = "Thank You for registering in adaptune for Vibrance16.. Kindly confirm your registeration by paying at our payment desks in the academic block portico, VIT Chennai Campus.. Regards, Team Vibrance.";
+	
 
 	$check_query = "SELECT * FROM adaptune WHERE email = '{$current_user}' ";
 	$check_result = mysqli_query($conn, $check_query);
@@ -26,10 +26,11 @@
 		$query = "INSERT INTO adaptune (name, email, college, regno, phno)";
 		$query .= " VALUES ('{$name}', '{$email}', '{$college}', '{$regno}', '{$phno}')";
 		$result = mysqli_query($conn, $query);
+		$message = "Thank You for registering in adaptune for Vibrance16.. Kindly confirm your registeration by paying at our payment desks in the academic block portico, VIT Chennai Campus.. Regards, Team Vibrance.";
+		mail ("prashant.bhardwaj2014@vit.ac.in", "Registration for adaptune", $message, "From: vibrance2016@gmail.com"); 
 
 	    if ($result) {
-	      	echo"You have succesfully registered for adaptune. Please check your email for registraion slip. Your registraion will only be confirmed after you make the payment at our registration desk.";		      	
-	      	mail ($email, "Registration for adaptune", $message, "From: vibrance2016@gmail.com");      	
+	      	echo"You have succesfully registered for adaptune. Please check your email for registraion slip. Your registraion will only be confirmed after you make the payment at our registration desk.";		
 	    } else {
 		   	echo"Registration failed.";
 	    }    
