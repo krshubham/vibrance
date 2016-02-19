b5b6e99c4a00c8b6866108a74149bfa6f59e34d2
prashant <pkpbhardwaj729@gmail.com>
2016-02-19 09:46:42 +0530

    changed databse

--- reg/includes/db_connection.php
+++ reg/includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "rrobotic_root");
+define("DB_USER", "root");
 define("DB_PASS", "KinG82++");
-define("DB_NAME", "rrobotic_r2robotics");
+define("DB_NAME", "vibrance");
 
 // Create connection
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
