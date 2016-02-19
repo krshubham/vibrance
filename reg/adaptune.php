b90efbf3492f226637b5d53c1a653abe5774677d
prashant <pkpbhardwaj729@gmail.com>
2016-02-19 12:11:33 +0530

    added user check

--- reg/adaptune.php
+++ reg/adaptune.php
@@ -1,27 +1,43 @@
-
-
-  <?php require_once("includes/session.php"); ?>
+<?php require_once("includes/session.php"); ?>
 <?php require_once("includes/db_connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
+
 <?php
-$username=$_SESSION['username'];
+	$current_user = $_SESSION["username"];
+	$name_query = "SELECT * FROM users WHERE username = '{$current_user}' LIMIT 1";
+	$name_result = mysqli_query($conn, $name_query);
+	confirm_query($name_result);
+	$name_title = mysqli_fetch_assoc($name_result);
 
+	$name = $name_title['name'];
+	$email = $current_user;
+	$college = $name_title['college'];
+	$regno = $name_title['regno'];
+	$phno = $name_title['phno'];
 
+	$check_query = "SELECT email FROM adaptune ";
+	$check_result = mysqli_query($conn, $check_query);
+	confirm_query($check_result);
+	while ($check_title = mysqli_fetch_assoc($check_result)) {
+		if ($check_title['email']==$email) {
+			echo "You have already registered for adaptune.";
+		} else {
+			$query = "INSERT INTO adaptune (name, email, college, regno, phno)";
+			$query .= " VALUES ('{$name}', '{$email}', '{$college}', '{$regno}', '{$phno}')";
+			$result = mysqli_query($conn, $query);
 
-  $query = "INSERT INTO adaptune (username)";
-		$query .= " VALUES ('{$username}')";
-		$result = mysqli_query($conn, $query);
-
-        if ($result) {
-          	echo"Your account created!";	       	
-	    } else {
-		   	echo"Profile account failed.";
-	    }  
-	    ?>      	
-   
+		    if ($result) {
+		      	echo"You have succesfully registered for adaptune. Please check your email for registraion slip. Your registraion will only be confirmed after you make the payment at our registration desk.";	       	
+		    } else {
+			   	echo"Registration failed.";
+		    }  
+		}
+	}  	
+?>      	
+  
 <?php
-if (isset ($conn)){
-  mysqli_close($conn);
-}
+	if (isset ($conn)){
+	  mysqli_close($conn);
+	}
 ?>
 
