e88384c02a28ae52332bc74de50d38527b64920b
Kumar Shubham <kumarshubham347@gmail.com>
2016-02-21 10:03:37 +0530

    Test 4

--- reg/admin/index.php
+++ reg/admin/index.php
@@ -47,8 +47,8 @@ if (isset($_POST['submit'])) {
     @import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
     @import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
     body {
-        margin: -10%;
-        padding: -10%;
+        margin: 0px;
+        padding: 0px;
         background: #fff;
         color: #fff;
         font-family: Arial;
