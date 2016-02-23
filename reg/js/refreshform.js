e54cf090301f139a50b92bca4629c58b6b1e9f04
prashant <pkpbhardwaj729@gmail.com>
2016-02-23 15:07:15 +0530

    last bug fix in parti logic2

--- reg/js/refreshform.js
+++ reg/js/refreshform.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#adaptune").click(function() { 
-    var event = $("#event").val();       
+    var event = $("#event").val();
+    var parti = $("#parti").val();       
         $.post("refreshform.php", {event1: event, parti1: parti}, function(data) {
             alert(data);                
         });
