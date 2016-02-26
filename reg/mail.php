3ce3af0fde90e0f4037f9041cd0528e6f4749f69
Kumar Shubham <kumarshubham347@gmail.com>
2016-02-26 16:44:42 +0530

    CHnages to bill

--- reg/mail.php
+++ reg/mail.php
@@ -1,6 +1,3 @@
-<?php require_once("includes/session.php"); ?>
-<?php require_once("includes/db_connection.php"); ?>
-<?php require_once("includes/functions.php"); ?>
 <?php
 	if (isset($_POST['submit'])) {
 		$email = "pkpbhardwaj729@gmail.com";
@@ -12,8 +9,4 @@
 <form action="mail.php" method="post">
 	<input type="submit" name="submit" value="submit">
 </form>
-<?php
-	if (isset ($conn)){
-	  mysqli_close($conn);
-	}
-?>
+
