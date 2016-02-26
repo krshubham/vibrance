5c6f2aec497574ce3deba90c027b4b398ea556b3
prashant <pkpbhardwaj729@gmail.com>
2016-02-26 11:55:02 +0530

    changed emails

--- reg/PHPMailer-master/test.php
+++ reg/PHPMailer-master/test.php
@@ -6,13 +6,13 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'amit@gmail.com';                   // SMTP username
-$mail->Password = 'digitalinspiration';               // SMTP password
+$mail->Username = 'pkpbhardwaj729@gmail.com';                   // SMTP username
+$mail->Password = 'NewOne11';               // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 $mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
-$mail->setFrom('amit@gmail.com', 'Amit Agarwal');     //Set who the message is to be sent from
+$mail->setFrom('pkpbhardwaj729@gmail.com', 'Amit Agarwal');     //Set who the message is to be sent from
 $mail->addReplyTo('labnol@gmail.com', 'First Last');  //Set an alternative reply-to address
-$mail->addAddress('pkpbhardwaj729@gmail.com', 'Josh Adams');  // Add a recipient
+$mail->addAddress('prashant.bhardwaj2014@vit.ac.in', 'Josh Adams');  // Add a recipient
 $mail->addAddress('ellen@example.com');               // Name is optional
 $mail->addCC('cc@example.com');
 $mail->addBCC('bcc@example.com');
