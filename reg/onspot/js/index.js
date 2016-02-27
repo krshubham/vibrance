c4b6c560c726788936c463cbc7d8ed37f6ac6fcf
Kumar Shubham <kumarshubham347@gmail.com>
2016-02-27 21:27:38 +0530

    lock test

--- reg/onspot/js/index.js
+++ reg/onspot/js/index.js
@@ -1,17 +1,4 @@
 // Input Lock
-$('textarea').blur(function () {
-    $('#hire textarea').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('textarea + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('textarea + label + span').css({'opacity': 0});
-        }
-    });
-});
 
 $('#hire .field:first-child input').blur(function () {
     $('#hire .field:first-child input').each(function () {
