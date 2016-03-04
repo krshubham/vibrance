c747dd461eda50d3736366f9c86db191ff083bc4
Kumar Shubham <kumarshubham347@gmail.com>
2016-03-04 21:06:08 +0530

    Index.php test for login

--- login/index.php
+++ login/index.php
@@ -114,7 +114,7 @@ if(isset($_POST['signup'])){
 	}
 </style>
 </head>  
-<body style="height: 100%; overflow: hidden; width: 100% !important;zoom: 90% !important;">
+<body style="height: 100%; width: 100% !important;zoom: 90% !important;">
 
 	<div id="fback"><div class="girisback"></div><div class="kayitback"></div></div>
 
