1b8f23bd5c3e01f2a68a6f44654c1d6beb3027ff
Kumar Shubham <kumarshubham347@gmail.com>
2016-03-17 11:48:42 +0530

    Added the meta tag for anaytics

--- js/script.js
+++ js/script.js
@@ -7,22 +7,6 @@ function isIE() {
     return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
 };
 
-/*===================Analytics====================*/
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-73543324-1', 'auto');
-        ga('send', 'pageview');
-//=========================================================
 /* cookie.JS
  ========================================================*/
 include('js/jquery.cookie.js');
