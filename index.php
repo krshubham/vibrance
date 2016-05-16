fecd531c39bc66f83b4faf0ba41ad051ff905628
Kumar Shubham <Kumar Shubham>
2016-05-16 15:18:16 +0530

    Added code

--- index.php
+++ index.php
@@ -53,24 +53,31 @@ if (isset($_SESSION["username"])) {
     <![endif]-->
     
     <script src='js/device.min.js'></script>
-    <!--*******************************Google analytics*****************************-->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-3900805761322774",
+        enable_page_level_ads: true
+    });
+</script>
+<!--*******************************Google analytics*****************************-->
+<script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-        ga('create', 'UA-73543324-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <!--******************google analytics end*******************************-->
+    ga('create', 'UA-73543324-1', 'auto');
+    ga('send', 'pageview');
+</script>
+<!--******************google analytics end*******************************-->
 </head>
 
 <body>
