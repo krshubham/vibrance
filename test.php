6408deed5cf91abfbcfd9b8c55ed08c25c7f10a2
Prashant <pkpbhardwaj729@gmail.com>
2016-07-19 21:23:18 +0530

    test db

--- test.php
+++ test.php
@@ -1,6 +1,18 @@
+<?php require_once("includes/session.php"); ?>
+<?php require_once("includes/db_connection.php"); ?>
+<?php require_once("includes/functions.php"); ?>
+
 <?php
 $inp = $_POST['inp'];
-echo $inp;
+$query = "INSERT INTO test (inp)";
+$query .= " VALUES ('{$inp}')";
+$result = mysqli_query($conn, $query);  
+
+$check_query = "SELECT * FROM test ";
+$check_result = mysqli_query($conn, $check_query);
+confirm_query($check_result);
+$check = mysqli_fetch_assoc($check_result);
+echo $check['inp'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,4 +24,5 @@ echo $inp;
 		<input type="text" name="inp">
 	</form>
 </body>
+<script type="text/javascript"></script>
 </html>
